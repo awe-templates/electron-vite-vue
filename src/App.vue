fce99da145a3d5f5cf6d18967cb34256815cb388
relliv <epsilonelittr@gmail.com>
2024-10-16 04:17:38 +0300

    Update texts

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Electron + Vite + Vue" />
+  <HelloWorld msg="Electron + Vite + Vue + TailwindCSS" />
   <div class="flex-center">
     Place static files into the <code>/public</code> folder
     <img style="width: 2.4em; margin-left: 0.4em" src="/logo.svg" alt="Logo" />
